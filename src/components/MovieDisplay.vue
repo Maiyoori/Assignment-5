d73a6b5fd523af6652a9a505ae3bd0977eb2d545
unknown <maiyoorisri@gmail.com>
2022-12-12 11:39:45 -0500

    modal

--- src/components/MovieDisplay.vue
+++ src/components/MovieDisplay.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from "vue";
-import axios from "axios";
+import axios from "axios";;
 
 const movie = ref("");
 const response = ref(null);
@@ -12,48 +12,64 @@ const getData = async (url, params) => {
     console.log(error);
   }
 };
-
+ 
 const getMovies1 = async () => {
-  movie.value = await getData("https://api.themoviedb.org/3/movie/popular", {
+  movie.value = await
+  getData(`https://api.themoviedb.org/3/movie/${movie_options.value}`, {
     params: {
       api_key: "53df15a69c17485812f9134bcd4089b6",
-    },
+      append_to_response: "videos", 
+    }
   });
-  console.log(movie);
-};
+ }
+
 </script>
-
+ 
 <template>
-  <body>
-    <button @click="getMovies1">Click me</button>
+<body>
 
-    <div v-if="movie" class="movie-container">
-      <p>Title: {{ movie.data.title }}</p>
-      <p>Overview: {{ movie.data.overview }}</p>
-      <p>Release Date: {{ movie.data.release_date }}</p>
-      <p>Genres: Action, Adventure, Science Fiction</p>
-      <p>Language: English</p>
-      <p>Vote Average: {{ movie.data.vote_average }}/10</p>
-      <p>Vote Count: {{ movie.data.vote_count }}</p>
-      <p>Popularity: {{ movie.data.popularity }}</p>
-      <p>Trailer:</p>
-      <img
-        class="poster"
-        :src="`https://image.tmdb.org/t/p/w500${movie.data.poster_path}`"
-        alt=""
-      />
-      <iframe
-        class="iframe"
-        :src="`https://www.youtube.com/embed/${
-          movie.data.videos.results.filter((video) => video.type === 'Trailer').at(0).key
-        }`"
-      ></iframe>
+  <div class="select">
+    <div class="select_text">
+      <label for="movie_options">Choose a movie:</label>
     </div>
-  </body>
+    <form class="movie_list">
+      <select v-model="response" id="movie_options">
+        <option value="24428" class="avengers">The Avengers</option>
+        <option value="99861">Avengers: Age of Ultron</option>
+        <option value="299536">Avengers: Infinity War</option>
+        <option value="299534">Avengers: Endgame</option>
+        <option value="1726">Iron Man</option>
+        <option value="10138">Iron Man 2</option>
+        <option value="68721">Iron Man 3</option>
+        <option value="1771">Captain America: The First Avenger</option>
+        <option value="100402">Captain America: The Winter Soldier</option>
+        <option value="271110">Captain America: Civil War</option>
+      </select>
+    </form>
+    <div class="button_align">
+      <button @click="getMovies1">Click me</button>
+
+    </div>
+    <div v-if="movie" class="movie-container">
+    <p>Title: {{ movie.data.title }}</p> 
+    <p>Overview: {{ movie.data.overview }}</p>
+    <p>Release Date: {{ movie.data.release_date }}</p>
+    <p>Genres: Action, Adventure, Science Fiction</p>
+    <p>Language: English</p>
+    <p>Vote Average: {{ movie.data.vote_average }}/10</p>
+    <p>Vote Count: {{ movie.data.vote_count }}</p>
+    <p>Popularity: {{ movie.data.popularity }}</p>
+    <p>Trailer:</p>
+    <img class="poster" :src="`https://image.tmdb.org/t/p/w500${movie.data.poster_path}`" alt="">
+    <iframe class="iframe" :src="`https://www.youtube.com/embed/${movie.data.videos.results.filter((video) => video.type === 'Trailer').at(0).key}`"></iframe>
+  </div>
+  </div>
+
+</body> 
 </template>
 
 <style>
-.comic {
+.comic{
   margin: -1%;
   margin-left: -0.3%;
   width: 100.3%;
@@ -63,7 +79,8 @@ const getMovies1 = async () => {
   z-index: -1;
 }
 
-.title_logo {
+
+.title_logo{
   display: flex;
   margin-left: -4%;
   margin-top: -15%;
@@ -71,45 +88,46 @@ const getMovies1 = async () => {
   justify-content: center;
 }
 
-#logo {
+#logo{
   z-index: 1;
   width: 20%;
   position: absolute;
   margin-top: 0px;
 }
 
-.select {
+.select{
   margin-top: 10%;
 }
 
-.select_text {
+.select_text{
   margin-top: -4px;
   font-size: x-large;
   text-align: center;
 }
 
-#movie_options {
+#movie_options{
   text-align: center;
   font-size: 17px;
 }
 
-.button_align {
+.button_align{
   margin-top: 0.5%;
   text-align: center;
 }
 
-.movie_list {
+.movie_list{
   text-align: center;
 }
 
-img {
+img{
   width: 38%;
   position: absolute;
   margin-left: 2%;
   margin-top: 80px;
 }
 
-.movie-container {
+
+.movie-container{
   position: absolute;
   font-size: 27px;
   font-weight: bold;
@@ -120,14 +138,14 @@ img {
   font-weight: 100;
 }
 
-.poster {
+.poster{
   width: 486px;
   height: 730px;
   margin-left: -580px;
   margin-top: 30px;
 }
 
-iframe {
+iframe{
   font-size: 21.5px;
   display: flex;
   margin-left: 14%;
